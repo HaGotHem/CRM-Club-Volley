b57ee97c398f6a685bde0d22088933e0411fc5ad
TevaLT <tevannd@gmail.com>
2026-06-17 12:37:39 +0200

    update dashboard

--- src/script/burgermenu.js
+++
@@ -1,6 +0,0 @@
-const burgerButton = document.getElementById('menu-toggle');
-const burgermenu = document.getElementById('burger-menu');
-
-burgerButton.addEventListener('click', function() {
-    burgermenu.classList.toggle('open');
-});
