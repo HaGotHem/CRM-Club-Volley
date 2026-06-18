257633ce03812b858d210b14ee3019317c97c471
HaGotHem <thomas.bertacch1510@gmail.com>
2026-06-18 11:53:05 +0200

    début du js pour rendre les boutons fonctionnels

---
+++ public/frontend/src/script/sync.js
@@ -0,0 +1,13 @@
+const weezsync = document.getElementById('btn-sync-WeezEvent');
+const brevosync = document.getElementById('btn-sync-Brevo');
+
+weezsync.addEventListener('click', async () => {
+    console.log('[syncWeezEvent] Démarrage de la synchronisation...');
+    // ... ton code de synchronisation ici
+});
+
+brevosync.addEventListener('click', async () => {
+    console.log('[syncBrevo] Démarrage de la synchronisation...');
+    // ... ton code de synchronisation ici
+});
+
